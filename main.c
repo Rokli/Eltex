8cd1732fd298d0e932058d0afd986fcf95009e83
hikiray <hikiray@192.168.0.14>
2024-07-10 04:06:56 +0700

    Add function AddPerson and add demo menu

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #define true 1
+#define false 0
 #define N 512
+
 struct Person {
     int id;
     char name[N];
@@ -13,6 +15,39 @@ struct Person {
     char socialNetwork[N];
 };
 
+struct Person AddPerson(){
+    struct Person person;
+
+    printf("Введите имя:");
+    scanf("%c", &person.name);
+
+    printf("Введите фамилию:");
+    scanf("%c", &person.surname);
+
+    printf("Введите отчество:");
+    scanf("%c", &person.patronymic);
+
+    return person;
+}
 int main(void){
-     
+    struct Person phoneBook[N];
+    char tmp[1];
+    int flag = 1;
+    while(flag){
+        printf("1 - Добавить пользователя \t 2- Вывести список пользователей\n3 - Выйти из телефонной книги\n");
+        scanf("%c", &tmp);
+        switch (atoi(tmp))
+        {
+        case 1:
+            phoneBook[1] = AddPerson();
+            break;
+        case 2:
+            printf("2");
+            break;
+        case 3:
+            flag = false;
+        default:
+            break;
+        }
+    }
 }
