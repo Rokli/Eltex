67976c0fa79e5b15e4ca92b2f0977abd7d0210ec
hikiray <hikiray@192.168.0.14>
2024-07-10 03:44:19 +0700

    Add struct Person

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#define true 1
+#define N 512
+struct Person {
+    int id;
+    char name[N];
+    char surname [N];
+    char patronymic[N];
+    char post[N];
+    char numberPhone[N];
+    char email[N];
+    char socialNetwork[N];
+};
 
-
-
-int main(){
-    printf("Hello 11World");
+int main(void){
+     
 }
