3a4ec120c99c051d34ecac8ebbc1d3ef81d1f2fb
hikiray <hikiray@192.168.0.14>
2024-07-16 23:43:32 +0700

    create folder 6.3

---
+++ 6.3/Models/Contact.h
@@ -0,0 +1,18 @@
+#ifndef contact_h
+#define contact_h
+
+#include "List.h"
+
+void AddPerson(List *phoneBook);
+
+void PrintPhoneBook(List *phoneBook);
+
+void EditContact(List *phoneBook);
+
+void EditContact(List *phoneBook);
+
+void DeleteContact(List *phoneBook);
+
+void PrintIndividualContact(List* phoneBook);
+
+#endif
