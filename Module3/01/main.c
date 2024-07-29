fe1b84bd4be5265e78b2064482cd497a827e0853
hikiray <shatalomaksimka12345678910@gmail.com>
2024-07-30 04:25:40 +0700

    added dynamic memory

--- Module3/01/main.c
+++ Module3/01/main.c
@@ -8,13 +8,13 @@ int calculateSquareArea(int sideLength) {
 
 int main(int argc, char *argv[]) {
 
-    if(argc ==1 ){
+    if(argc == 1 ){
         printf("Использование: %s <длина стороны n> ... \n", argv[0]);
         return 1;
     }
-    
-    int sides[argc-1],pid;
-    int middle = argc / 2;
+    int n = argc-1;
+    int *sides = (int*)malloc(n * sizeof(int));
+    int middle = argc / 2,pid;
 
     for(int i = 1; i < argc; i++){
         sides[i-1] = atoi(argv[i]);
@@ -26,15 +26,19 @@ int main(int argc, char *argv[]) {
             exit(1); 
         case 0:
             printf("Процесс-потомок:\n");
+
             for(int i = 0; i < middle; i++){
                 printf("Площадь квадрата:%d\tСторона:%d\n",calculateSquareArea(sides[i]),sides[i]);
             }
+            
             break;
         default:
             printf("Процесс-родитель:\n");
+
             for(int i = middle; i < argc-1; i++){
                 printf("Площадь квадрата:%d\tСторона:%d\n",calculateSquareArea(sides[i]),sides[i]);
             }
+
             break;
 }
     return 0;
