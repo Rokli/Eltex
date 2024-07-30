6e8932e6ec55227bd91207c40a134f8cdd5da1f8
hikiray <shatalomaksimka12345678910@gmail.com>
2024-07-31 03:04:45 +0700

    refactor code

--- Module3/08/main.c
+++ Module3/08/main.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+#include <wait.h>
+#include <time.h>
 
 #define MAX_LENGTH_CHAR 128
 
-void stop(int semid) {
-    struct sembuf p = {0, -1, 0}; 
-    semop(semid, &p, 1);
+union semun {
+    int val; /* значение для SETVAL */
+    struct semid_ds *buf; /* буферы для IPC_STAT, IPC_SET */
+    unsigned short *array; /* массивы для GETALL, SETALL */
+                           /* часть, особенная для Linux: */
+    struct seminfo *__buf; /* буфер для IPC_INFO */
+};
+
+void P(int semid){
+    struct sembuf lock_res = {0, -1, 0};
+    semop(semid, &lock_res, 1);
 }
 
-void start(int semid) {
-    struct sembuf v = {0, 1, 0}; 
-    semop(semid, &v, 1);
+void V(int semid){
+    struct sembuf rel_res = {0, 1, 0};
+    semop(semid, &rel_res, 1);
 }
-
 int main(int argc, char *argv[]) {
-    key_t key = ftok("/etc/fstab", getpid());; 
-    int semid = semget(key, 3, IPC_CREAT | 0666); 
-    
+    key_t key = ftok("module3", getpid());
+    int semid;
+    semid = semget(key, 0, 0666 | IPC_CREAT);
+
     semctl(semid, 0, SETVAL, 1); 
-    
+
     int pipe_fd[2];
     if (pipe(pipe_fd) == -1) {
         perror("pipe");
         return 1;
     }
-
+    
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork");
@@ -40,11 +47,11 @@ int main(int argc, char *argv[]) {
     }
     
     if (pid == 0) { 
+        srand(time(NULL));
         close(pipe_fd[0]); 
         while (1) { 
-            sleep(1);
-            stop(semid); 
-
+            sleep(2);
+            P(semid);
             FILE *file = fopen("random_numbers.txt", "r");
             char name[MAX_LENGTH_CHAR];
 
@@ -59,35 +66,35 @@ int main(int argc, char *argv[]) {
             
             fclose(file); 
 
-            srand(time(NULL)); 
-            int random_number = rand() % 100; 
+            int random_number = rand() % 500;
             write(pipe_fd[1], &random_number, sizeof(random_number));
-            start(semid);
+            V(semid);
         }
         close(pipe_fd[1]); 
         return 0;
     } else { 
         close(pipe_fd[1]); 
         while (1) { 
-            sleep(2);
-            stop(semid);
+            sleep(3);
+            P(semid);
+            int received_number;
+
             FILE *file = fopen("random_numbers.txt", "a");
             if (file == NULL) {
                 perror("Файл не открылся");
                 exit(1); 
             }
 
-            int received_number;
             read(pipe_fd[0], &received_number, sizeof(received_number));
             fprintf(file, "%d\n", received_number);
             
             fclose(file); 
-
-            start(semid);
+            
+            V(semid);
         }
         wait(NULL); 
         close(pipe_fd[0]); 
     }
-    semctl(semid, 0, IPC_RMID);
+
     return 0;
 }
