473d9a98c803d5183f0e6be027bcb6b8a4859f31
hikiray <shatalomaksimka12345678910@gmail.com>
2024-08-02 02:21:36 +0700

    added creation of a set of random numbers

--- Module3/12/main.c
+++ Module3/12/main.c
@@ -1,45 +1,110 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-#define SHM_KEY 123
-#define SIZE_SHM sizeof(int) 
+#define KEY 123
+#define SIZE_SHM sizeof(int)
 
-int main(){
+void P(int semid) {
+  struct sembuf lock_res = {0, -1, 0};
+  semop(semid, &lock_res, 1);
+}
 
-    int shmid = shmget(SHM_KEY, SIZE_SHM, IPC_CREAT | 0666);
+void V(int semid) {
+  struct sembuf rel_res = {0, 1, 0};
+  semop(semid, &rel_res, 1);
+}
 
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
+int MaxNum(int *array,int size_array) {
+  int max = array[1];
+  for (int i = 1; i < size_array; i++) {
+    if (array[i] > max) max = array[i];
+  }
+  return max;
+}
+
+int MinNum(int *array,int size_array) {
+  int min = array[1];
+  for (int i = 1; i < size_array; i++) {
+    if (array[i] < min) min = array[i];
+  }
+  return min;
+}
+
+int main() {
+  int semid = semget(KEY, 1, IPC_CREAT | 0666);
+  int shmid = shmget(KEY, SIZE_SHM, IPC_CREAT | 0666);
+
+  semctl(semid, 0, SETVAL, 1);
+
+  if (shmid == -1) {
+    perror("shmget");
+    exit(1);
+  }
+
+  int *shared_memory = (int *)shmat(shmid, NULL, 0);
+  if (shared_memory == (int *)-1) {
+    perror("shmat");
+    exit(1);
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    perror("fork");
+    return 1;
+  }
+
+  srand(time(NULL));
+  
+  if (pid == 0) {
+    while(1){
+        P(semid);
+        int size_array = shared_memory[0];
+        shared_memory[1] = MaxNum(shared_memory,size_array);
+        shared_memory[2] = MinNum(shared_memory,size_array);
+        printf("Дочерний процесс отработал\n");
+        V(semid);
+        sleep(1);
     }
 
-    int *shared_memory = (int *)shmat(shmid, NULL, 0);
-    if (shared_memory == (int*)-1) {
-      perror("shmat");
-      exit(1);
-    }
+  } else {
+    for (int i = 0;; i++) {
+       int size_array = rand() % 10;
+      if (i%2 == 0) {
+        P(semid);
+       
 
-    pid_t pid = fork();
+        for (int i = 1; i < size_array; i++) {
+          shared_memory[i] = rand() % 15;
+        }
+        shared_memory[i] = size_array;
+        printf("отработал\n");
+        V(semid);
+      } else {
 
-    if(pid == -1){
-        perror("fork");
-        return 1;
-    }
-    
-    if(pid == 0){
-        *shared_memory = 5;
-    }else{
-        wait(NULL);
-        printf("Сообщение из разделяемой памяти: %d\n", *shared_memory);
-    }
+        sleep(1);
+        P(semid);
+        for (int i = 0; i < size_array; i++) {
+          printf("Элемент %d: %d\n", i, shared_memory[i]);
+        }
+        printf("Максимальное значение:%d\n",shared_memory[1]);
+        printf("Минимальное значение:%d\n",shared_memory[2]);
+        V(semid);
+      }
 
-    shmdt(shared_memory); 
-    shmctl(shmid, IPC_RMID, NULL); 
-    return 0;
+      
+    }
+  }
+
+  shmdt(shared_memory);
+  shmctl(shmid, IPC_RMID, NULL);
+  return 0;
 }
